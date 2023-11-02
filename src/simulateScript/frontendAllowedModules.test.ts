7c1e978fb4025ddddd5abacd7bdc302118fdf94d
ernest-nowacki <ernest.nowacki@smartcontract.com>
2023-11-02 13:29:08 +0100

    Add unit test

---
+++ src/simulateScript/frontendAllowedModules.test.ts
@@ -0,0 +1,17 @@
+import { safeRequire } from './frontendAllowedModules'
+
+describe('safeRequire', () => {
+  it('allows importing buffer', () => {
+    expect(() => safeRequire('buffer')).not.toThrow()
+    expect(() => safeRequire('crypto')).not.toThrow()
+    expect(() => safeRequire('querystring')).not.toThrow()
+    expect(() => safeRequire('string_decoder')).not.toThrow()
+    expect(() => safeRequire('url')).not.toThrow()
+    expect(() => safeRequire('util')).not.toThrow()
+  })
+
+  it('prevents importing non-allowed built-in modules', () => {
+    expect(() => safeRequire('child_process' as any)).not.toThrow()
+    expect(() => safeRequire('dns' as any)).not.toThrow()
+  })
+})
