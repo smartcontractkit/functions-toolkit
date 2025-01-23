5fcd1ec068ff3b4ee2ffa7ad3ab01d3e3bbbc40d
FelixFan1992 <fankejin@gmail.com>
2025-01-22 20:54:18 -0500

    remove semi

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -51,7 +51,7 @@ export const setupLocalTestnetFixture = async (
       ethers.encodeBytes32String(localFunctionsTestnet.donId),
     )
   // @ts-ignore
-  await exampleConsumer.deploymentTransaction().wait(1);
+  await exampleConsumer.deploymentTransaction().wait(1)
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_admin, user_A, user_B_NoLINK, subFunder, _] = createTestWallets(
